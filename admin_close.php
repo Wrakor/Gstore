a2934ba72a0219cff4c98daebdcf2c2834c9118d
Ricardo Coutinho <ricardocoutinho86@gmail.com>
2015-03-14 02:26:09 +0000

    no message

---
+++ admin_close.php
@@ -0,0 +1,13 @@
+        <!-- jQuery -->
+        <script src="js/jquery.min.js"></script>
+
+        <!-- Bootstrap Core JavaScript -->
+        <script src="bootstrap/js/bootstrap.min.js"></script>
+
+        <!-- SB Admin Morris Charts JavaScript -->
+        <script src="startbootstrap-sb-admin-1.0.2/js/plugins/morris/raphael.min.js"></script>
+        <script src="startbootstrap-sb-admin-1.0.2/js/plugins/morris/morris.min.js"></script>
+        <script src="startbootstrap-sb-admin-1.0.2/js/plugins/morris/morris-data.js"></script>
+
+    </body>
+</html>
--- admin_nav.php
+++ admin_nav.php
@@ -11,7 +11,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand brand" href="index.php">G<b>STORE</b></a>
+            <a class="navbar-brand brand" href="backoffice.php">G<b>STORE</b>Admin</a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
@@ -41,10 +41,7 @@
                 </li>
             </ul>
             <ul class="nav navbar-nav navbar-right">
-                <li><a href="backoffice.php"><span class="glyphicon glyphicon-lock"></span> Admin </a></li>
-                <li><a href="cart.php"><span class="badge">2</span><span class="glyphicon glyphicon-shopping-cart"></span> Cart </a></li>
-                <li><a href="profile.php"><span class="glyphicon glyphicon-user"></span> Profile </a></li>
-                <li><a href="#" data-toggle="modal" data-target="#login"><span class="glyphicon glyphicon-log-in"></span> Login </a></li>
+                <li><a href="index.php"><span class="glyphicon glyphicon-home"></span> Return to Store </a></li>
                 <li><a href="" data-toggle="modal" data-target="#logout"><span class="glyphicon glyphicon-off"></span> Logout </a></li>
             </ul>
         </div><!-- /.navbar-collapse -->
